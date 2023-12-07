22fdc0a8f09bd9b557a9c8a2f33c1f37bc2d7e3d
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-07 17:27:36 -0400

    Delete payment module and related files

--- src/model/payment/dto/create-payment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsString } from 'class-validator';
-
-export class CreatePaymentDto {
-  @IsNumber()
-  amount: number;
-  @IsString()
-  currency: string;
-  description: string;
-}

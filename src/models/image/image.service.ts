bea97cb5399ab62182f88865ec1c335d2bdbb382
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-07 17:42:46 -0400

    Add image module and controller

---
+++ src/models/image/image.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateImageDto } from './dto/create-image.dto';
+// import { UpdateImageDto } from './dto/update-image.dto';
+import { PrismaService } from 'src/common/database/prisma.service';
+
+@Injectable()
+export class ImageService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateImageDto[]): Promise<any> {
+    const images = data.map((imageUrl) => ({
+      ...imageUrl,
+    }));
+    return this.prisma.image.createMany({
+      data: images,
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.image.findMany();
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.image.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  // async update(id: string, data: UpdateImageDto) {}
+
+  remove(id: string) {
+    return `This action removes a #${id} image`;
+  }
+}
